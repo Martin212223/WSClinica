34958b17aa623871216bef260e593cd0aa5fb3db
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-20 16:37:33 -0300

    controller agregado

---
+++ WSClinica/Migrations/20230120192639_regex.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WSClinica.Migrations
+{
+    public partial class regex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Medico_Especialidades_EspecialidadId",
+                table: "Medico");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pacientes_Medico_MedicoId",
+                table: "Pacientes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pacientes",
+                table: "Pacientes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Especialidades",
+                table: "Especialidades");
+
+            migrationBuilder.RenameTable(
+                name: "Pacientes",
+                newName: "Paciente");
+
+            migrationBuilder.RenameTable(
+                name: "Especialidades",
+                newName: "Especialidad");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pacientes_MedicoId",
+                table: "Paciente",
+                newName: "IX_Paciente_MedicoId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Paciente",
+                table: "Paciente",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Especialidad",
+                table: "Especialidad",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Medico_Especialidad_EspecialidadId",
+                table: "Medico",
+                column: "EspecialidadId",
+                principalTable: "Especialidad",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Paciente_Medico_MedicoId",
+                table: "Paciente",
+                column: "MedicoId",
+                principalTable: "Medico",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Medico_Especialidad_EspecialidadId",
+                table: "Medico");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Paciente_Medico_MedicoId",
+                table: "Paciente");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Paciente",
+                table: "Paciente");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Especialidad",
+                table: "Especialidad");
+
+            migrationBuilder.RenameTable(
+                name: "Paciente",
+                newName: "Pacientes");
+
+            migrationBuilder.RenameTable(
+                name: "Especialidad",
+                newName: "Especialidades");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Paciente_MedicoId",
+                table: "Pacientes",
+                newName: "IX_Pacientes_MedicoId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pacientes",
+                table: "Pacientes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Especialidades",
+                table: "Especialidades",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Medico_Especialidades_EspecialidadId",
+                table: "Medico",
+                column: "EspecialidadId",
+                principalTable: "Especialidades",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pacientes_Medico_MedicoId",
+                table: "Pacientes",
+                column: "MedicoId",
+                principalTable: "Medico",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ WSClinica/Models/Paciente.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSClinica.Models
+{
+    [Table("Paciente")]
+    public class Paciente
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Apellido { get; set; }
+
+        public int NroHistClinica { get; set; }
+
+        public int MedicoId { get; set; }
+
+        [ForeignKey("MedicoId")]
+        public Medico Medico { get; set; }
+    }
+}
